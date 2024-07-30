46c89e31f0794a5dcae05225bfe1d57945dba315
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-30 15:55:44 +0200

    creato secondo metodo esercizio2

--- src/esercizio2/Main.java
+++ src/esercizio2/Main.java
@@ -6,18 +6,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        int N = 10;
+        List<Integer> listaCasuale = generaListaRandom(N);
+        System.out.println(listaCasuale);
     }
 
+    //METODO PER GENERARE LISTA CON VALORI RANDOMICI DA 0 A 100
     public static List<Integer> generaListaRandom(int num) {
         //creo la lista di interi
         List<Integer> lista = new ArrayList<>();
 
         for (int i = 0; i < num; i++) {
             //aggiungo un numero randomico da 0 a 100 tante volte quanto il valore di num
-            lista.add((int) Math.random() * 101);
+            lista.add((int) (Math.random() * 101));
         }
         Collections.sort(lista);
         return lista;
+    }
+
+    public static List<Integer> duplicaListaInOrdineInverso(List<Integer> lista) {
+        List<Integer> nuovaLista = new ArrayList<>(lista);
+        List<Integer> reverse = new ArrayList<>(lista);
+
+        //inverto l'ordine degli elementi
+        Collections.reverse(reverse);
+
+        //aggiungo alla lista la reverse
+        nuovaLista.addAll(reverse);
+        return nuovaLista;
+
     }
 }
